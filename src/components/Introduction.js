680852f874880230142d06cd8bc0d40e912cdf15
Aaron <aaron@sevnasoftware.com>
2023-04-04 19:32:16 +0100

    Added Introduction component

---
+++ src/components/Introduction.js
@@ -0,0 +1,8 @@
+export default function Introduction() {
+    return (
+        <div className="section">            
+            <h5 className="section-title section-header">Introduction</h5>
+            <p className="section-content">A driven and highly focused Software Engineer who has acquired extensive experience in delivering key solutions within leading organisations. An individual with a sustained record of success utilising C# with .Net Core and .Net Framework, Typescript with Angular and VueJS, and C++ on Windows and Linux across a broad array of complex projects deployed to desktop, server, mobile, and web platforms. An individual who possesses good interpersonal skills; with an ability to communicate effectively and build professional relationships. An experienced and driven professional able to participate effectively in a team and use their own initiative to ensure deliverables are achieved. A flexible individual who easily adapts to new environments and enjoys the challenges of meeting deadlines/targets: now seeking a new challenge where skills and diverse strengths will bring immediate value within a forward-thinking organisation.</p>
+        </div>
+    )
+}
