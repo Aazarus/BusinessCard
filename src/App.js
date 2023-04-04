a4baa54aa8d5ff3386e16d28c157a387045e39cd
Aaron <aaron@sevnasoftware.com>
2023-04-04 19:33:51 +0100

    Updated app component to use card to display content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
+// CSS
 import './App.css';
+
+// Components
+import AboutMe from './components/AboutMe';
+import Introduction from './components/Introduction';
+import Proficiencies from './components/Proficiencies';
+
+// Bootstrap
+import Card from 'react-bootstrap/Card';
+import me from './images/me.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Card className="app">
+        <Card.Img src={me}>
+        </Card.Img>
+        <Card.Body>
+          <AboutMe />
+          <Introduction />
+          <Proficiencies />
+        </Card.Body>
+      </Card>
     </div>
   );
 }
